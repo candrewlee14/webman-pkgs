068b2a6889416d1c0805eab758c79281d7dbd291
John Olheiser <john.olheiser@gmail.com>
2022-05-24 17:50:14 -0500

    Updates for is_raw_binary (#20)
    * Update is_raw_binary
    
    Signed-off-by: jolheiser <john.olheiser@gmail.com>
    
    * Add exe to windows
    
    Signed-off-by: jolheiser <john.olheiser@gmail.com>

--- full-bintest.sh
+++ full-bintest.sh
@@ -2,4 +2,5 @@
 
 for file in pkgs/*; do
   go run github.com/candrewlee14/webman@main -l "." dev bintest "$(echo "$file" | sed -E 's|pkgs\/([^.]+).*|\1|')"
+  # webman -l "." dev bintest "$(echo "$file" | sed -E 's|pkgs\/([^.]+).*|\1|')"
 done
